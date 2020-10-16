c83df9dbaed2d4fab1ad089159c80bcda88c4c6f
CrustySean <cattoni.elia@icloud.com>
2020-10-16 18:23:53 +0200

    cleanup untested features

--- src/main.cpp
+++ src/main.cpp
@@ -4,17 +4,11 @@
 #include <fstream>
 #include <memory>
 #include <filesystem>
-#include <vector>
-#include <iostream>
 #include <switch.h>
-
-#include <Utility.hpp>
 
 void printInfo()
 {
     printf("BigCatAndTea give you a warm welcome!\n\n");
-    printf(" \n"); // make some space between text
-    printf("X : Backup News(IMPORTANT!)\n");
     printf("Y : Install News found in sdmc:/bcat\n");
     printf("+ : Exit\n");
 }
@@ -46,82 +40,6 @@ void BCATinstaller()
     }
 }
 
-/*Result NewsManager::Install(NewsArchive* archive)
-{
-    Result rc;
-    
-    return rc;
-}*/
-
-
-void NewsBackup()
-{
-    Result rc = 0;
-    NewsDatabaseService db;
-    std::vector<NewsArchive> archives;
-    
-    /* Open database service. */
-    if (R_SUCCEEDED(rc = newsCreateNewsDatabaseService(&db))) {
-        do {
-            /* Count entries. */
-            u32 count = 0;
-            if (R_FAILED(rc = newsDatabaseCount(&db, "", &count)))
-                break;
-             archives.reserve(count);
-
-            /* Read records. */
-            u32 records_read = 0;
-            std::vector<NewsRecord> records(count);
-            if (R_FAILED(rc = newsDatabaseGetList(&db, records.data(), count, "", "", &records_read, 0)))
-                break;
-            
-            /* Sanity check. */
-            if (records_read != count) {
-                printf("record count mismatch: %d vs %d\n", count, records_read);
-                break;
-            }
-     
-            for (auto &record : records) {
-                NewsDataService data;
-                if (R_FAILED(rc = newsCreateNewsDataService(&data)))
-                    continue;
-
-                do {
-                    if (R_FAILED(rc = newsDataOpenWithNewsRecord(&data, &record)))
-                        break;
-
-                    u64 archiveSize = 0;
-                    if (R_FAILED(rc = newsDataGetSize(&data, &archiveSize)))
-                        break;
-
-                    u64 bytes_read;
-                    std::vector<u8> buffer(archiveSize);
-                    if (R_FAILED(rc = newsDataRead(&data, &bytes_read, 0, buffer.data(), buffer.size())))
-                        break;
-
-                    archives.emplace_back(record.news_id, std::move(buffer)); // here is is our msgpack
-                } while (false);
-
-                if (R_FAILED(rc))
-                    printf("failed to read record %s with 0x%x\n", record.news_id, rc);
-
-                newsDataClose(&data);
-            }
-        } while (false);
-
-        /* Close service. */
-        newsDatabaseClose(&db);
-
-    }
-
-    if (R_FAILED(rc)) {
-        printf("Failed to retreive remote news: 0x%x\n", rc);
-    } else {
-        printf("Heh we actually did it\n", rc);
-    }
-}
-
-
 int main(int argc, char **argv)
 {
     consoleInit(NULL);
@@ -142,7 +60,6 @@ int main(int argc, char **argv)
 
         if (kDown & KEY_PLUS) break;
         if (kDown & KEY_Y) BCATinstaller();
-        if (kDown & KEY_X) NewsBackup();
         consoleUpdate(NULL);
     }
 
